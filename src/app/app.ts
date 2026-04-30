1f35e9c84b114f5e6414301705f6152323787441
Collin Koldoff <collinkoldoff@gmail.com>
2026-04-29 21:22:44 -0500

    Added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
 import { Hero } from './hero/hero';
 import { Projects } from './projects/projects';
 import { Skills } from './skills/skills';
@@ -7,7 +8,7 @@ import { Skills } from './skills/skills';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Hero, RouterOutlet, Projects, Skills],
+  imports: [Hero, RouterOutlet, Projects, Skills, Footer],
   templateUrl: './app.html',
 })
 export class App {}
